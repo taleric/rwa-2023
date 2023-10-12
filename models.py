442381fe92f325af39f655830b378b791331e652
taleric <95088056+taleric@users.noreply.github.com>
2023-10-12 22:30:41 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,22 +1,35 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
-class UserIn(BaseModel):
-    username: str
-    email: str
-    password: str
 
 class UserDb(BaseModel):
     username: str = Field(alias="_id")
     email: str
     hashed_password: str
 
-class TodoIn(BaseModel):
-    title: str
-    description: Optional[str]
 
-class TodoDb(BaseModel):
-    id: str = Field(alias="_id")
+class UserIn(BaseModel):
     username: str
+    email: str
+    password: str
+
+
+class PostIn(BaseModel):
     title: str
-    description: Optional[str]
+    slug: str
+    content: Optional[str]
+    published: bool = Field(default=False)
+
+
+class PostDb(PostIn):
+    id: str = Field(alias="_id")
+
+
+class CommentIn(BaseModel):
+    comment: str
+
+
+class CommentDb(CommentIn):
+    id: str = Field(alias="_id")
+    post_id: str
+    username: str
